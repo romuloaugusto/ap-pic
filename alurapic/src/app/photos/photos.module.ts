b6359d2315938f1f622cb87442aef9c2b002a3ac
Romulo Agusto da Silva <romulo.augusto@RP10609.semace.local>
2018-10-11 15:13:50 -0300

    initial commit

---
+++ alurapic/src/app/photos/photos.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
+import { CommonModule } from "@angular/common";
+
+import { PhotoListComponent } from './photo-list/photo-list.component';
+import { PhotoComponent } from "./photo/photo.component";
+import { PhotoFormComponent } from './photo-form/photo-form.component';
+
+@NgModule({
+    declarations: [ 
+        PhotoComponent, 
+        PhotoListComponent, 
+        PhotoFormComponent 
+    ],
+    exports: [ PhotoComponent ],
+    imports: [ 
+        HttpClientModule,
+        CommonModule
+     ]
+})
+export class PhotosModule {}
